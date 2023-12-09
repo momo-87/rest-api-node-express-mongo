a22b6da358d8b07f224aa70c40d7e12ced791474
Momo-87 <momochristian87@gmail.com>
2023-12-09 03:18:09 +0100

    update the comment in the index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 // Create express instance
 const app = express();
 
-// listen for requests 
+// listen for requests (process.env.port is if we deploy on platform Heroku, which uses an environment variable to set the port)
 app.listen(process.env.port || 4000, () => {
   console.log('now listening for requests');
 });
