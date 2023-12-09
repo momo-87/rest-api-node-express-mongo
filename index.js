b8ce678607dd8636a0ef9b2cf38843ee59980fbc
Momo-87 <momochristian87@gmail.com>
2023-12-09 03:33:42 +0100

    Create the GET method route

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import express from 'express';
 // Create express instance
 const app = express();
 
+app.get('/api', (req, res) => {
+  console.log('GET request');
+  res.send({nome: 'Momo'});
+});
+
+
 // listen for requests (process.env.port is if we deploy on platform Heroku, which uses an environment variable to set the port)
 app.listen(process.env.port || 4000, () => {
   console.log('now listening for requests');
