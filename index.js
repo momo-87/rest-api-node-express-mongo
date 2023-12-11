e91f880de76b4fdc123367e97a2363e9cfcf720e
Momo-87 <momochristian87@gmail.com>
2023-12-11 15:33:02 +0100

    handle error

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ app.use(bodyParser.json());
 // initilize routes
 app.use('/api', routes);
 
+// error handling middleware
+app.use((err, req, res, next) => {
+  // console.log(err);
+  res.status(422).send({error: err.message});
+});
+
 // listen for requests (process.env.port is if we deploy on platform Heroku, which uses an environment variable to set the port)
 app.listen(process.env.port || 4000, () => {
   console.log('now listening for requests');
--- routes/api.js
+++ routes/api.js
@@ -4,24 +4,24 @@ import User from '../models/user.js';
 const router = express.Router();
 
 // get a list of users from the db
-router.get('/users', (req, res) => {
+router.get('/users', (req, res, next) => {
   res.send({type: 'GET'});
 });
 
 // add a new user to the db
-router.post('/users', (req, res) => {
+router.post('/users', (req, res, next) => {
   User.create(req.body).then((user) => {
     res.send(user);
-  });
+  }).catch(next); // catch any errors, call the next middleware in the chain (see index.js)
 });
 
 // update a user in the db
-router.put('/users/:id', (req, res) => {
+router.put('/users/:id', (req, res, next) => {
   res.send({type: 'PUT'});
 });
 
 // delete a user from the db
-router.delete('/users/:id', (req, res) => {
+router.delete('/users/:id', (req, res, next) => {
   res.send({type: 'DELETE'});
 });
 
