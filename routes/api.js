6a8197a3b0c1d02169470fe9d85ba690395fbedf
Momo-87 <momochristian87@gmail.com>
2023-12-12 22:15:44 +0100

    get house by coordinate and distance

--- routes/api.js
+++ routes/api.js
@@ -4,12 +4,39 @@ import House from '../models/house.js';
 const router = express.Router();
 
 // get a list of Houses from the db
-router.get('/house', (req, res, next) => {
-  res.send({type: 'GET'});
+router.get('/houses', (req, res, next) => {
+  // Get all existing houses
+  // House.find({}).then((house) => {
+  //   res.send(house);
+  // });
+
+  // get all houses based on the provided coordinates and maxDistance
+  const longitude = parseFloat(req.query.long);
+  const latitude = parseFloat(req.query.lat);
+
+  // Define the geoNear aggregation pipeline
+  const geoNearPipeline = [
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [longitude, latitude]
+        },
+        distanceField: 'dist.calculated',
+        maxDistance: 100000,
+        spherical: true
+      },
+    },
+  ];
+
+  House.aggregate(geoNearPipeline)
+    .then((houses) => {
+      res.send(houses);
+    }).catch(next);
 });
 
 // add a new house to the db
-router.post('/house', (req, res, next) => {
+router.post('/houses', (req, res, next) => {
   House.create(req.body).then((house) => {
     res.send(house);
   }).catch(next); // catch any errors, call the next middleware (error handling middleware) in the chain (see index.js)
